a91bbcb84afab280fce50ed846675f881e7fbc4e
Udit H <udith1306@gmail.com>
2026-04-21 12:52:26 +0530

    Updated

--- forking.c
+++ forking.c
@@ -1,21 +1,22 @@
 #include<sys/types.h>
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
 
 int main()
 {
     pid_t pid;
-    pid = fork();
-    if(pid < 0)
+    pid = fork(); //Fork a child process
+    if(pid < 0) //Error Occured
     {
         fprintf(stderr, "Fork Failed");
         return 1;
     }
-    else if(pid == 0)
+    else if(pid == 0) //Child process
     {
         execlp("/bin/ls", "ls", NULL);
     }
-    else{
+    else{ //Parent Process
         wait(NULL);
         printf("Child Complete");
     }
